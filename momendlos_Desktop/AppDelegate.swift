969bea5553a7b803af58b469a6cd1601e6fb83f4
Felix Döring <Doering.Felix@googlemail.com>
2015-06-03 12:04:04 +0200

    Initial Commit

---
+++ momendlos_Desktop/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  momendlos_Desktop
+//
+//  Created by Felix Döring on 03/06/15.
+//  Copyright (c) 2015 Felix Döring. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
